5366e6936979b2318c33ee94619c1576ab63f763
Dominik Chrastecky <dominik@chrastecky.cz>
2024-03-07 11:17:10 +0100

    Add settings component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),
-  }
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.component').then(m => m.SettingsComponent),
+  },
 ];
