bfd71e88f40fed5f998b521d30730aad41a63c37
Dominik Chrastecky <dominik@chrastecky.cz>
2024-03-07 11:40:49 +0100

    Add some form / col styles

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {TranslocoPipe} from "@ngneat/transloco";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {AuthManagerService} from "../../services/auth-manager.service";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslocoPipe,
+    ReactiveFormsModule
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  public form = new FormGroup({
+    apiKey: new FormControl<string>(this.authManager.anonymousApiKey),
+  });
 
+  constructor(
+    private readonly authManager: AuthManagerService,
+  ) {
+  }
 }
