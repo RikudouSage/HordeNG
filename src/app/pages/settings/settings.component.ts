27a3a1e3e3d25ee03c0f70a58c14a214693d9210
Dominik Chrastecky <dominik@chrastecky.cz>
2024-03-07 20:22:26 +0100

    Add settings

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,9 +1,12 @@
-import {Component, computed, signal} from '@angular/core';
+import {Component, computed, OnInit, signal} from '@angular/core';
 import {TranslocoPipe} from "@ngneat/transloco";
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthManagerService} from "../../services/auth-manager.service";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
+import {AiHorde} from "../../services/ai-horde.service";
+import {toPromise} from "../../helper/resolvable";
+import {LoaderComponent} from "../../components/loader/loader.component";
 
 @Component({
   selector: 'app-settings',
@@ -11,12 +14,13 @@ import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
   imports: [
     TranslocoPipe,
     ReactiveFormsModule,
-    FaIconComponent
+    FaIconComponent,
+    LoaderComponent
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
   private iconEyeOpen = signal(faEye);
   private iconEyeClosed = signal(faEyeSlash);
   public passwordVisible = signal(false);
@@ -28,16 +32,43 @@ export class SettingsComponent {
     return this.iconEyeOpen();
   });
 
+  public loading = signal(true);
+
   public form = new FormGroup({
-    apiKey: new FormControl<string>(this.authManager.anonymousApiKey),
+    apiKey: new FormControl<string>(this.authManager.anonymousApiKey, [Validators.required]),
   });
 
   constructor(
     private readonly authManager: AuthManagerService,
+    private readonly horde: AiHorde,
   ) {
+  }
+
+  public async ngOnInit(): Promise<void> {
+    this.form.patchValue({
+      apiKey: this.authManager.apiKey(),
+    });
+    this.loading.set(false);
   }
 
   public async togglePasswordVisibility(): Promise<void> {
     this.passwordVisible.update(visible => !visible);
+  }
+
+
+  public async submitForm(): Promise<void> {
+    if (!this.form.valid) {
+      return;
+    }
+    this.loading.set(true);
+    const previous = this.authManager.apiKey();
+    this.authManager.apiKey = this.form.controls.apiKey.value!;
+    const response = await toPromise(this.horde.currentUser());
+    if (!response.success) {
+      this.authManager.apiKey = previous;
+      // todo show message
+    }
+
+    this.loading.set(false);
   }
 }
