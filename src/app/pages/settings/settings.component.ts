b36793ddfbd99a8e056acd8ae9ad4216bf5907a3
Dominik Chrastecky <dominik@chrastecky.cz>
2024-03-07 17:44:32 +0100

    Toggleable password visibility

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {TranslocoPipe} from "@ngneat/transloco";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {AuthManagerService} from "../../services/auth-manager.service";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
   imports: [
     TranslocoPipe,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FaIconComponent
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  private iconEyeOpen = signal(faEye);
+  private iconEyeClosed = signal(faEyeSlash);
+  public passwordVisible = signal(false);
+  public passwordFieldIcon = computed(() => {
+    if (this.passwordVisible()) {
+      return this.iconEyeClosed();
+    }
+
+    return this.iconEyeOpen();
+  });
+
   public form = new FormGroup({
     apiKey: new FormControl<string>(this.authManager.anonymousApiKey),
   });
@@ -21,5 +35,9 @@ export class SettingsComponent {
   constructor(
     private readonly authManager: AuthManagerService,
   ) {
+  }
+
+  public async togglePasswordVisibility(): Promise<void> {
+    this.passwordVisible.update(visible => !visible);
   }
 }
