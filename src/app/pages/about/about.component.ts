a4829d87062d580cfe0145cccce8235b29193f20
Dominik Chrastecky <dominik@chrastecky.cz>
2024-03-18 21:50:56 +0100

    Add runtime variables

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {TranslocoMarkupComponent} from "ngx-transloco-markup";
+import {environment} from "../../../environments/environment";
+import {TranslocoPipe} from "@ngneat/transloco";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
-    TranslocoMarkupComponent
+    TranslocoMarkupComponent,
+    TranslocoPipe
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  public version = signal(environment.appVersion);
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
+if (typeof window === 'undefined') {
+  (<any>global).window = <any>{};
+}
+
 export const environment = {
   appName: 'HordeNG',
-  appVersion: '0.0.1',
+  appVersion: (<any>window)?.['APP_VERSION'] ?? '0.0.1',
   maintainer: 'Rikudou',
   apiUrl: 'https://aihorde.net/api',
   apiVersion: 'v2',
